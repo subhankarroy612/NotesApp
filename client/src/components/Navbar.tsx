5ecab7b835e2568c928cf9bd64f8528a92e6b17f
Subhankar Roy <subhankarroy612@gmail.com>
2023-03-28 19:15:38 +0530

    added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -48,15 +48,13 @@ export default function Navbar() {
     return (
         <>
             <Box bg='#ff004c' px={4} position={'sticky'} top={0} zIndex={1000}>
-                <Box display={'flex'} h={10} alignItems={'center'} justifyContent={'space-between'}>
-
+                <Flex  h={10} alignItems={'center'} justifyContent={'space-between'}>
                     <Box display={'flex'} gap={8}
-                        //  spacing={8}
                         alignItems={'center'}>
                         <Box color={'white'} as='b'>NotesApp</Box>
                     </Box>
                     <Box alignItems={'center'}>
-                        <Menu>
+                        {/* <Menu>
                             <MenuButton
                                 as={Button}
                                 rounded={'full'}
@@ -71,9 +69,9 @@ export default function Navbar() {
                                 <MenuDivider />
                                 <MenuItem onClick={handleLogout}>Logout</MenuItem>
                             </MenuList>
-                        </Menu>
+                        </Menu> */}
                     </Box>
-                </Box>
+                </Flex>
 
             </Box>
         </>
